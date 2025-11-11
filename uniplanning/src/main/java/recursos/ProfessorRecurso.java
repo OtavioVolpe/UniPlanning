8b7557e101f59724959d811d835d0b43a71b222f
Otávio Volpe Leal <126003350+OtavioVolpe@users.noreply.github.com>
2025-11-11 01:34:02 +0000

    Configurando o Codespace e POST

--- uniplanning/src/main/java/recursos/ProfessorRecurso.java
+++ uniplanning/src/main/java/recursos/ProfessorRecurso.java
@@ -4,14 +4,21 @@ import java.util.List;
 
 import entidades.Professor;
 import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("professor")
 public class ProfessorRecurso {
     @GET
-    public List<Professor> listar () {
+    public List<Professor> listar() {
         return Professor.listAll(Sort.ascending("nome"));
     }
 
+    @POST
+    @Transactional
+    public void salvar(Professor professor) {
+        professor.persist();
+    }
 }
