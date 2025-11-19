846964a57a7f3fc29ead5d02a5dd98fd0df5cb28
Otávio Volpe Leal <126003350+OtavioVolpe@users.noreply.github.com>
2025-11-19 18:33:50 +0000

    Todos DELETE e PUT feitos

--- uniplanning/src/main/java/recursos/PlannerRecurso.java
+++ uniplanning/src/main/java/recursos/PlannerRecurso.java
@@ -5,9 +5,12 @@ import java.util.List;
 import entidades.Planner;
 import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("planner")
 public class PlannerRecurso {
@@ -20,5 +23,24 @@ public class PlannerRecurso {
     @Transactional
     public void salvar(Planner planner){
         planner.persist();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void delete(@PathParam("id") Integer id){
+        Planner.deleteById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public void editar(@PathParam("id") Integer id, Planner plannerAtualizado){
+        Planner plannerExistente = Planner.findById(id);
+
+        plannerExistente.prioridades = plannerAtualizado.prioridades;
+        plannerExistente.agenda = plannerAtualizado.agenda;
+        plannerExistente.tarefas = plannerAtualizado.tarefas;
+        plannerExistente.aluno = plannerAtualizado.aluno;
     }
 }
--- uniplanning/src/main/java/recursos/ProgressoRecurso.java
+++ uniplanning/src/main/java/recursos/ProgressoRecurso.java
@@ -5,9 +5,12 @@ import java.util.List;
 import entidades.Progresso;
 import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("progresso")
 public class ProgressoRecurso {
@@ -20,6 +23,23 @@ public class ProgressoRecurso {
     @Transactional
     public void salvar(Progresso progresso) {
         progresso.persist();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void delete(@PathParam("id") Integer id){
+        Progresso.deleteById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public void editar(@PathParam("id") Integer id, Progresso progressoAtualizado){
+        Progresso progressoExistente = Progresso.findById(id);
+
+        progressoExistente.disciplina = progressoAtualizado.disciplina;
+        progressoExistente.aluno = progressoAtualizado.aluno;
     }
 
 }
